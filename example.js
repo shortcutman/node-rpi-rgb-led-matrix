ed8f3cd48b58c6a905e20db1bda6ba23eeda9240
Daniel Burke <danieljburke@me.com>
2018-07-28 23:53:00 +0000

    Report the width and height of the connected LED Matrix.

--- example.js
+++ example.js
@@ -3,6 +3,9 @@ var LedMatrix = require(".");
 var matrix = new LedMatrix(16, 2, 1);
 matrix.fill(255, 0, 0);
 
+console.log("reported width: " + matrix.getWidth() );
+console.log("reported height: " + matrix.getHeight() );
+
 setTimeout(function () {
 	matrix.clear();
 }, 1500);
